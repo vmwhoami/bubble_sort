75fba6042f2d83c2dbfe0a77e8ffbc3aad3f46b2
vmwhoami <vitalie.m@travod.com>
2022-07-14 00:38:25 +0300

    Fix rubocop yml  file

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,16 @@
 def bubble_sort(arr)
   loop do
-    sorted = true
-    (arr.length - 1).times { |i| sorted = sorter(arr, i) if arr[i] > arr[i + 1] }
-    break if sorted
+    sorted = false
+    (arr.length - 1).times { |int| sorted = sorter(arr, int) if arr[int] > arr[int + 1] }
+    break unless sorted
   end
   arr
 end
 
-def sorter(arr, i)
-  arr[i], arr[i + 1] = arr[i + 1], arr[i]
+def sorter(arr, int)
+  arr[int], arr[int + 1] = arr[int + 1], arr[int]
 
-  false
+  true
 end
 p bubble_sort([3, 1, 7, 1, 9, 6, 8, 8])
 p bubble_sort([12, 1, 1, 13, 45, 1.2, 18, 213])
